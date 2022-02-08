541557e83a17f4a0b9441ab8b6ff0ff37b097554
tiagoebone <tiagoebone@gmail.com>
2022-02-07 22:49:07 -0300

    adiciona commit com teste aleatório

--- src/routes/expenses.tsx
+++ src/routes/expenses.tsx
@@ -1,12 +1,16 @@
-import { Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
+import React from "react";
 
 export default function Expenses() {
+  const [resposta, setResposta] = React.useState("");
+
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Expenses</h2>
-      <Button variant="outlined" onClick={() => alert("opa bão")}>
-        Opa bão
+      <Button variant="outlined" onClick={() => setResposta("Opa, bão sim")}>
+        <Typography variant="h6">Opa bão?</Typography>
       </Button>
+      <h3>{resposta}</h3>
     </main>
   );
 }
