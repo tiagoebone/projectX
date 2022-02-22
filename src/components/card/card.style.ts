7cb10f0620039eeff2638e5de93ceedbe2967651
tiagoebone <tiagoebone@gmail.com>
2022-02-21 23:22:58 -0300

    adiciona conteudo para os cards

---
+++ src/components/card/card.style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import VerifiedIcon from "@mui/icons-material/Verified";
+
+export const IconVerified = styled(VerifiedIcon)`
+  color: #b0b2ff;
+  transition: 200ms;
+  height: 18px;
+`;
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,15 +1,78 @@
+import Grid from "@mui/material/Grid";
+import { IconVerified } from "./card.style";
+
 const Card = () => {
   return (
     <div
       style={{
-        height: "110px",
+        height: "20vh",
+        minHeight: "110px",
+        maxHeight: "210px",
         backgroundColor: "#fff",
         borderRadius: "6px",
-        padding: "4px",
+        padding: "16px",
         boxShadow: "0px 3px 10px 2px rgba(0, 0, 0, 0.4)",
       }}
     >
-      Aqui vai ficar o conteúdo
+      <Grid container style={{ height: "100%" }}>
+        <div
+          style={{
+            height: "100%",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <img
+            style={{ height: "100%", borderRadius: "6px" }}
+            src="https://i.redd.it/zipsd227ktx61.jpg"
+            alt=""
+          />
+          <div
+            style={{
+              marginLeft: "16px",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              <div>
+                <span
+                  style={{
+                    fontSize: "24px",
+                    lineBreak: "auto",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Which of this stadiums are the most beautiful in the world?
+                </span>
+              </div>
+              <div style={{ marginTop: "8px" }}>
+                <span style={{ fontSize: "16px", lineBreak: "auto" }}>
+                  Football is not only an aggressive sport. It can be beautiful.
+                  Design of the football arena can either boost the mood of
+                  football players. So, go through and vote for the most
+                  beautiful one.
+                </span>
+              </div>
+            </div>
+
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <IconVerified />
+              <span
+                style={{
+                  fontSize: "14px",
+                  lineBreak: "auto",
+                  color: "#b0b2ff",
+                }}
+              >
+                Official content
+              </span>
+            </div>
+          </div>
+        </div>
+      </Grid>
     </div>
   );
 };
